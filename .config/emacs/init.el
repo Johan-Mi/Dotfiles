8ecccb76761d38d8ca8bd958aa456ce2f2132cfa
Johan-Mi <johanmi@protonmail.com>
2024-05-03 19:58:13 +0200

    Emacs: install `evil-god-state`

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -33,6 +33,13 @@
   :config
   (evil-collection-init))
 
+(use-package evil-god-state
+  :ensure t
+  :after evil
+  :config
+  (evil-define-key 'normal global-map " " 'evil-execute-in-god-state)
+  (evil-define-key 'god global-map [escape] 'evil-god-state-bail))
+
 (use-package which-key
   :ensure t
   :config
