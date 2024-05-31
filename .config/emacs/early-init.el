6cc61774483b3eba5a9a9f98039feabd15ec0107
Johan-Mi <johanmi@protonmail.com>
2024-05-31 17:33:12 +0200

    Emacs: speed up startup by deferring GC

---
+++ .config/emacs/early-init.el
@@ -0,0 +1,7 @@
+;; Minimize garbage collection during startup
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Lower threshold back to 8 MiB (default is 800kB)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (expt 2 23))))
