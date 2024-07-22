87f53977bcaf0b7543a36ea720e9cf1b94e21e6a
Johan-Mi <johanmi@protonmail.com>
2024-07-22 16:16:45 +0200

    Emacs: hide GUI elements during startup

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -5,3 +5,7 @@
 (add-hook 'emacs-startup-hook
           (lambda ()
             (setq gc-cons-threshold (expt 2 23))))
+
+;; Hide GUI elements during startup
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
